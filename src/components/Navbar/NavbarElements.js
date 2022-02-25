33d4056449db370f40e50e0cefdc52c426222111
nima <nimaqanbari1999@gmail.com>
2022-02-25 07:21:38 -0800

    add productDataTwo and Footer

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,40 +1,41 @@
-import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
-import { FaPizzaSlice } from "react-icons/fa"
+import styled from "styled-components";
+import {FaPizzaSlice } from 'react-icons/fa'
 
 export const Nav = styled.nav`
-background-color: transparent;
-height: 80px;
-display: flex ;
-justify-content: center;
-font-weight: 700;
-`
+  background: transparent;
+  display: flex;
+  justify-content: center;
+  height: 80px;
+  font-weight: 700;
+`;
 
 export const NavLink = styled(Link)`
-color: #fff;
-font-size: 2rem;
-display: flex;
-align-items: center;
-text-decoration: none;
-cursor: pointer;
+  color: #fff;
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  cursor: pointer;
 
-@media screen and (max-width:400px) {
+  @media screen and (max-width: 400px) {
     position: absolute;
     top: 10px;
     left: 25px;
-}
-`
+  }
+`;
+
 
 export const NavIcon = styled.div`
 display: block;
 position: absolute;
-top:0 ;
+top: 0;
 right: 0;
 cursor: pointer;
 color: #fff;
 
-p {
-    transform: translate(-175%, 100%);
+p{
+    transform: translate(-175% , 100%);
     font-weight: bold;
 }
 
@@ -42,6 +43,5 @@ p {
 
 export const Bars = styled(FaPizzaSlice)`
 font-size: 2rem;
-transform: translate(-50%, -15%);
-
+transform: translate(-50% , -15%);
 `
