46833a1518e09ae59d1fb4a55d27673877a6fd62
nima <nimaqanbari1999@gmail.com>
2022-02-25 03:55:43 -0800

    add products

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import {BrowserRouter as Router } from 'react-router-dom'
-import Hero from './components/Hero'
 import { GlobalStyle } from './globalStyles'
+import Hero from './components/Hero'
+import Products from './components/Products'
+import { productData } from './components/Products/data'
 
 const App = () => {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading="Choose your favorite " data={productData}/>
     </Router>
   )
 }
